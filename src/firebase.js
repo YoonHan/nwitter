d41ba1760ec4f1208b2131401589a1916bcd920a
yoonhan <hyku2014@gmail.com>
2020-12-20 13:29:18 +0900

    Add .env

--- src/firebase.js
+++ src/firebase.js
@@ -13,14 +13,14 @@ import "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAjQZq-8Gbed6UjXPZntDYuoGZPaJo08Uw",
-  authDomain: "nwitter-f04ec.firebaseapp.com",
-  projectId: "nwitter-f04ec",
-  storageBucket: "nwitter-f04ec.appspot.com",
-  messagingSenderId: "198903696715",
-  appId: "1:198903696715:web:e89672b0b071def3092cce",
-  measurementId: "G-XEV6QK7BSX"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+export default firebase.initializeApp(firebaseConfig);
 firebase.analytics();
